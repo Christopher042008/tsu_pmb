41b159e179d777b089997dc8c44990b6d147b0ca
Bertha <bertha@tsu.ac.id>
2026-03-03 13:44:29 +0700

    update model

---
+++ sources/app/Models/User/BerkasPendaftaran.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\User;
+
+use App\Models\MasterData\Master_Soal;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class BerkasPendaftaran extends Model
+{
+    // use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $table = 'pmb_berkas_pendaftaran';
+    protected $primaryKey = 'id';
+    // protected $fillable = [
+    //     'nik',
+    //     'role_access',
+    //     'password',
+    //     'created_at',
+    //     'created_by',
+    //     'updated_at',
+    //     'updated_by',
+    // ];
+}
